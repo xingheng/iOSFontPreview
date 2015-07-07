f188918a0bdbe653b976ae0d84e855e2cfd67e81
Wei Han <xingheng907@hotmail.com>
2015-07-07 10:17:21 +0800

    Initial Commit

---
+++ iOSFontPreview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSFontPreview
+//
+//  Created by WeiHan on 7/7/15.
+//  Copyright (c) 2015 Will Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSFontPreview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSFontPreview
+//
+//  Created by WeiHan on 7/7/15.
+//  Copyright (c) 2015 Will Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
